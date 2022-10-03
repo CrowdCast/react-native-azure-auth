230a9a06e2a02dcc6aad32adf0b8190d624d78b1
Camilo <camilo@crowdcast.jp>
2022-10-03 15:35:29 +0900

    Fix remove lisener

--- src/webauth/agent.js
+++ src/webauth/agent.js
@@ -14,13 +14,13 @@ export default class Agent {
         return new Promise((resolve, reject) => {
             const urlHandler = (event) => {
                 NativeModules.AzureAuth.hide()
-                Linking.removeEventListener('url', urlHandler)
+                // Linking.removeEventListener('url', urlHandler)
                 resolve(event.url)
             }
             const params = Platform.OS === 'ios' ? [ephemeralSession, closeOnLoad] : [closeOnLoad]
-            Linking.addEventListener('url', urlHandler)
+            const subscription = Linking.addEventListener('url', urlHandler);
             NativeModules.AzureAuth.showUrl(url, ...params, (err, redirectURL) => {
-                Linking.removeEventListener('url', urlHandler)
+                subscription.remove();
                 if (err) {
                     reject(err)
                 } else if(redirectURL) {
